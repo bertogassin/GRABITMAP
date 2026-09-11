12b7ea61e8811e9b39006cd42c117f0bd5188f70
bertogassin <bertogassin@email.com>
2026-09-11 17:50:06 +0200

    chore(release): bump version to 7.10.2

--- static/resursmap-sw.js
+++ static/resursmap-sw.js
@@ -2,7 +2,7 @@
 
 const CACHE_PREFIX = "grabit-shell-";
 const LEGACY_CACHE_PREFIX = "resursmap-shell-";
-const CACHE_VERSION = CACHE_PREFIX + "v7.10.1";
+const CACHE_VERSION = CACHE_PREFIX + "v7.10.2";
 
 const STATIC_ASSETS = [
     "/static/manifest.webmanifest",
